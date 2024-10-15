04d6e76b55e8ed5483e90aad7e2444fb33dfd558
Youngchae Chee <litcoderr@gmail.com>
2024-10-15 11:48:57 +0900

    impl: add modified gymnasium to support random starting point of lunarlander v2

--- rl_roboclip/roboclip.py
+++ rl_roboclip/roboclip.py
@@ -1,1 +1,9 @@
-# TODO implement ppo training
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.distributions as distributions
+
+import numpy as np
+import gymnasium as gym
+import matplotlib.pyplot as plt
